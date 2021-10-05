4f2e6428f3cef5844a478132c5b79926cec82786
Sabry Chasseray <sabry.chasseray@protonmail.com>
2021-10-05 23:53:24 +0200

    First rewrite commit, starting from scratch

--- Main.cpp
+++ Main.cpp
@@ -4,15 +4,9 @@
 
 int main()
 {
-	static auto mainSoulkanTestResult = sk::mainSoulkanTest(DEBUG);
-	static sk::SkTestData testData    = sk::retLog(mainSoulkanTestResult);
+	bool testValid = sk::graphic_test();
 
-	std::cout << "Total number of frames elapsed      : " << testData.totalFrames << std::endl;
-	std::cout << "Total time elapsed (in seconds)     : " << testData.totalTimeElapsed << std::endl;
-	std::cout << "Init time elapsed (in seconds)      : " << testData.initTimeElapsed << std::endl;
-	std::cout << "Average number of frames per second : " << testData.averageFramesPerSecond << std::endl;
-	std::cout << "Average frametime (in ms)           : " << testData.averageFrametime << std::endl;
-	std::cout << "Debug mode is                       : " << (DEBUG ? "ON" : "OFF") << std::endl;
+	std::cout << "Test is " << (testValid ? "valid" : "not valid") << std::endl;
 
-	return 1;
+	return EXIT_SUCCESS;
 }
